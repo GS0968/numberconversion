413d494369b79c6c876e8f1db7e79e162e80d50c
GS0968 <gagan.s.govind@gmail.com>
2025-10-22 14:44:21 +0900

    Implement hex conversion function in dentohex.py

---
+++ dentohex.py
@@ -0,0 +1,19 @@
+def convert(s):
+    hexlist= ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
+    hexstring=""
+    remainder=[]
+    quotient= int(int(s)/16)
+    remaindervalue=int(s)%16
+    remainder.append(remaindervalue)
+    while quotient!=0:
+        remaindervalue=quotient%16
+        quotient=int(quotient/16)
+        remainder.append(remaindervalue)
+    length=len(remainder)
+    while length>0:
+        length-=1
+        remaindervalue=int(remainder[length])
+        hexstring=hexstring+hexlist[remaindervalue]
+    return hexstring
+if __name__=="__main__":
+    convert()
