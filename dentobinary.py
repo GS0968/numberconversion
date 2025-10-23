795788bdfe337386ff5e0a0068765738857d5219
hmm274 <106000988+hmm274@users.noreply.github.com>
2025-10-23 11:28:22 +0900

    Minor redundancy fixes
    Simplified logic in variable assignment and while condition logic

--- dentobinary.py
+++ dentobinary.py
@@ -1,16 +1,7 @@
-def main():
-    Denary = int(input("Input the denary number you want to convert: "))
-    print(f"The binary of {Denary} is: {DenToBin(Denary)}")
-
 def DenToBin(A):
-    A=int(A)
-    Binary = str(A%2)
-    while True:
-        A = int(A/2)
-        Binary = str(A%2) + Binary
-        if int(A/2) == 0:
-            break
-    return(Binary)
-
-if __name__=="__main__":
-    main()
+    den=int(A)
+    Binary = str(den%2)
+    while int(den/2)!=0:
+        den = int(den/2)
+        Binary = str(den%2) + Binary
+    return Binary
