82e374f9ae1b8257dfe15be5f9124c85154ff35c
hmm274 <106000988+hmm274@users.noreply.github.com>
2025-10-23 11:01:47 +0900

    Updated conversion logic
    Eliminated use of very redundant case statement to implement a logical calculation

--- binarytohex.py
+++ binarytohex.py
@@ -8,45 +8,15 @@ def bintohex(Initial_value):
     i = len(Initial_value)
     while i > 0:
         val1 = Initial_value[i-4 : i]
-        match val1:
-            case "0000":
-                Char="0"
-            case"0001":
-                Char="1"
-            case"0010":
-                Char="2"
-            case"0011":
-                Char="3"
-            case "0100":
-                Char="4"
-            case"0101":
-                Char="5"
-            case"0110":
-                Char="6"
-            case"0111":
-                Char="7"
-            case"1000":
-                Char="8"
-            case"1001":
-                Char="9"
-            case"1010":
-                Char="A"
-            case"1011":
-                Char="B"
-            case"1100":
-                Char="C"
-            case"1101":
-                Char="D"
-            case"1110":
-                Char="E"
-            case"1111":
-                Char= "F"
+        digit = 0
+        for j in range(len(val1)):
+            digit = digit + int(list(val1)[-j-1])*(2**j)
+        if digit >= 10:
+            chars = ["A","B","C","D","E","F"]
+            Char = chars[digit-10]
+        else:
+            Char = str(digit)
         final_val = Char + final_val
         i = i - 4
 
     return(final_val)
-
-
-    
-      
-
