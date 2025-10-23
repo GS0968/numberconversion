52e006a3b3468a78f2130cf060c5b02b2e91652d
hmm274 <106000988+hmm274@users.noreply.github.com>
2025-10-23 11:48:33 +0900

    Added logic without imports

--- hextoden.py
+++ hextoden.py
@@ -1,7 +1,8 @@
-import hextobin
-import binarytoden
-
 def convert(s):
-    value=hextobin.convert(s)
-    denaryvalue=binarytoden.bin_den(value)
-    return denaryvalue
+    denary=0
+    for index in range(len(s)):
+        digit = s[-index-1]
+        if s[-index-1] not in range(9):
+            digit = ["A","B","C","D","E","F"].index(s[-index-1])+10
+        denary = denary + int(digit)*(16**index)
+    return denary
